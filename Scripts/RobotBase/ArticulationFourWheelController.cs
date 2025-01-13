8cd1732119660bad96aa02ebe17ca57961f6a2eb
BlackTea12 <BlackTea12@github.com>
2025-01-13 18:58:10 +0900

    [update] four wheel control script updated and tested

--- Scripts/RobotBase/ArticulationFourWheelController.cs
+++ Scripts/RobotBase/ArticulationFourWheelController.cs
@@ -6,23 +6,67 @@ using UnityEngine;
 public enum ControlMode { InPhase=0, OppositPhase, PivotTurn };
 public class ArticulationFourWheelController : MonoBehaviour
 {
-    public ArticulationBody flWheel, frWheel, rlWheel, rrWheel;
-    public ArticulationBody flSteer, frSteer, rlSteer, rrSteer;
+    [Header("[Wheel Gameobjects]")]
+    public ArticulationBody flWheel; 
+    public ArticulationBody  frWheel;
+    public ArticulationBody  rlWheel;
+    public ArticulationBody  rrWheel;
 
+    [Header("[Steering Gameobjects]")]
+    public ArticulationBody flSteer;
+    public ArticulationBody  frSteer;
+    public ArticulationBody  rlSteer;
+    public ArticulationBody  rrSteer;
+
+    [Header("[Driving Options]")]
     public ControlMode drivingMode = ControlMode.OppositPhase;
-    public float linearVelocityX, linearVelocityY, angularVelocity;    // [m/s] and [rad/s]
+    
+    [Header("[Maximum Value]")]
+    [Tooltip("[m/s]")]
+    public float linearVelocityX;
+    [Tooltip("[m/s]")]
+    public float linearVelocityY;
+    [Tooltip("[deg/s]")]
+    public float angularVelocity;
 
     /// <ranger unique value>
     private readonly float wheel_base = 0.364f, wheel_radius = 0.1f, wheel_steering_y_offset = 0.05f; //wheel_seperation = 0.494f
     private readonly float steering_track = 0.394f; // (=wheel_seperation - 2f*wheel_steering_y_offset)
-    public readonly float[] vel = { 0, 0, 0, 0 };
-    public readonly float[] pose = { 0, 0, 0, 0 };
+    
+    [Header("[Observations]")]
+    public readonly float[] vel = { 0.0f, 0.0f, 0.0f, 0.0f };
+    public readonly float[] pose = { 0.0f, 0.0f, 0.0f, 0.0f };
 
     void Start() {}
 
     void FixedUpdate() 
     {
-        SetRobotVelocity(linearVelocityX, angularVelocity);
+        float limit_vx = linearVelocityX;   // (m/s)
+        float limit_vy = linearVelocityY;   // (m/s)
+        float limit_w = angularVelocity*Mathf.PI/180.0f; // (rad/s)
+        float[] result = UserControl(limit_vx, limit_w);
+        SetRobotVelocity(result[0], result[1]);
+    }
+
+    public float[] UserControl(float max_vx, float max_w)
+    {
+        float inputX = 0.0f, inputY=0.0f;
+        float[] result = {0.0f, 0.0f}; // linear speed [m/s], angular speed [rad/s]
+        if (Input.GetKey(KeyCode.W)) {
+            inputX = 1.0f;
+        } else if (Input.GetKey(KeyCode.S)) {
+            inputX = -1.0f;
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            inputY = 1.0f;
+        } else if (Input.GetKey(KeyCode.A)) {
+            inputY = -1.0f;
+        }
+
+        result[0] = max_vx*inputX;
+        result[1] = max_w*inputY;
+        return result;
     }
 
     public void SetRobotVelocity(float targetLinearSpeed, float targetAngularSpeed)
@@ -31,13 +75,13 @@ public class ArticulationFourWheelController : MonoBehaviour
         {
             case ControlMode.InPhase:
             {
-                float linearVel = new Vector2(linearVelocityX, linearVelocityY).sqrMagnitude;
-                float sign = Mathf.Sign(linearVelocityX);
+                float linearVel = new Vector2(targetLinearSpeed, linearVelocityY).sqrMagnitude;
+                float sign = Mathf.Sign(targetLinearSpeed);
                 float angular = 0f;
                 
-                if (linearVelocityX != 0.0f)
+                if (targetLinearSpeed != 0.0f)
                 {
-                    angular = linearVelocityY / linearVelocityX;
+                    angular = linearVelocityY / targetLinearSpeed;
                 }
                 vel[0] = sign*linearVel;
                 vel[1] = sign*linearVel;
@@ -52,18 +96,32 @@ public class ArticulationFourWheelController : MonoBehaviour
 
             case ControlMode.OppositPhase:
             {
-                float vel_steerring_offset = angularVelocity * wheel_steering_y_offset;
-                float sign = Mathf.Sign(linearVelocityX);
-                float left_ele = new Vector2(linearVelocityX - angularVelocity*steering_track/2, angularVelocity*wheel_base/2).sqrMagnitude;
-                float right_ele = new Vector2(linearVelocityX + angularVelocity*steering_track/2, angularVelocity*wheel_base/2).sqrMagnitude;
+                // compute speed
+                float vel_steerring_offset = (targetAngularSpeed * wheel_steering_y_offset)/wheel_radius;
+                float sign = Mathf.Sign(targetLinearSpeed);
+                float denom = targetAngularSpeed*wheel_base/2.0f, numer = targetAngularSpeed*steering_track/2.0f;
+                float left_ele = new Vector2(targetLinearSpeed - numer, denom).sqrMagnitude / wheel_radius;
+                float right_ele = new Vector2(targetLinearSpeed + numer, denom).sqrMagnitude / wheel_radius;
                 vel[0] = sign*left_ele - vel_steerring_offset;
                 vel[1] = sign*right_ele + vel_steerring_offset;
-                vel[2] = sign*left_ele - vel_steerring_offset;
-                vel[3] = sign*right_ele + vel_steerring_offset;
-                pose[0] = Mathf.Atan(angularVelocity*wheel_base/(2f*linearVelocityX+angularVelocity*steering_track));
-                pose[1] = Mathf.Atan(angularVelocity*wheel_base/(2f*linearVelocityX + angularVelocity*steering_track));
-                pose[2] = -pose[0];
-                pose[3] = -pose[1];
+                vel[2] = vel[0];
+                vel[3] = vel[1];
+
+                // compute steering
+                if (Mathf.Abs(targetAngularSpeed) > 0.001f) {
+                    float twice_speed = 2.0f*targetLinearSpeed, track_angular = targetAngularSpeed*steering_track;
+                    if (Mathf.Abs(twice_speed) > Mathf.Abs(track_angular)) {
+                        pose[0] = Mathf.Atan(targetAngularSpeed*wheel_base/(twice_speed - track_angular));
+                        pose[1] = Mathf.Atan(targetAngularSpeed*wheel_base/(twice_speed + track_angular));
+                    } else {
+                        pose[0] = Mathf.Sign(targetAngularSpeed)*Mathf.PI/2.0f;
+                        pose[1] = pose[0];
+                    }
+                    pose[2] = -pose[0];
+                    pose[3] = -pose[1];
+                } else {
+                    pose[0] = pose[1] = pose[2] = pose [3] = 0.0f;
+                }
             }
             break;
             
@@ -74,8 +132,8 @@ public class ArticulationFourWheelController : MonoBehaviour
                 pose[2] = Mathf.Atan(wheel_base/steering_track);
                 pose[3] = -Mathf.Atan(wheel_base/steering_track);
                 
-                vel[0] = -angularVelocity;
-                vel[1] = angularVelocity;
+                vel[0] = -targetAngularSpeed;
+                vel[1] = targetAngularSpeed;
                 vel[2] = vel[0];
                 vel[3] = vel[1];
             }
@@ -95,35 +153,24 @@ public class ArticulationFourWheelController : MonoBehaviour
             break;
         }
         
-        SetWheelVelocity(flWheel, vel[0] / wheel_radius * Mathf.Rad2Deg);
-        SetWheelVelocity(frWheel, vel[1] / wheel_radius * Mathf.Rad2Deg);
-        SetWheelVelocity(rlWheel, vel[2] / wheel_radius * Mathf.Rad2Deg);
-        SetWheelVelocity(rrWheel, vel[3] / wheel_radius * Mathf.Rad2Deg);
-        SetWheelPosition(flSteer, pose[0] * Mathf.Rad2Deg);
-        SetWheelPosition(frSteer, pose[1] * Mathf.Rad2Deg);
-        SetWheelPosition(rlSteer, pose[2] * Mathf.Rad2Deg);
-        SetWheelPosition(rrSteer, pose[3] * Mathf.Rad2Deg);
-        // Stop the wheel if target velocity is 0
-        // if (targetLinearSpeed == 0 && targetAngularSpeed == 0)
-        // {
-        //     StopWheel(leftWheel);
-        //     StopWheel(rightWheel);
-        // }
-        // else
-        // {
-        //     // Convert from linear x and angular z velocity to wheel speed
-        //     vRight = targetAngularSpeed*(wheelTrackLength/2) + targetLinearSpeed;
-        //     vLeft = -targetAngularSpeed*(wheelTrackLength/2) + targetLinearSpeed;
+        // set values
+        Debug.Log($"vel: {vel[0]}, {vel[1]}, {vel[2]}, {vel[3]}");
+        Debug.Log($"pose: {pose[0]}, {pose[1]}, {pose[2]}, {pose[3]}");
+        SetWheelVelocity(flWheel, vel[0]);
+        SetWheelVelocity(frWheel, vel[1]);
+        SetWheelVelocity(rlWheel, vel[2]);
+        SetWheelVelocity(rrWheel, vel[3]);
+        SetWheelPosition(flSteer, pose[0]);
+        SetWheelPosition(frSteer, pose[1]);
+        SetWheelPosition(rlSteer, pose[2]);
+        SetWheelPosition(rrSteer, pose[3]);
 
-        //     SetWheelVelocity(leftWheel, vLeft / wheelRadius * Mathf.Rad2Deg);
-        //     SetWheelVelocity(rightWheel, vRight / wheelRadius * Mathf.Rad2Deg);
-        // }
     }
     /// <param name="jointVelocity">deg/s</param>
     private void SetWheelVelocity(ArticulationBody wheel, float jointVelocity)
     {
         ArticulationDrive drive = wheel.xDrive;
-        drive.target += jointVelocity * Time.fixedDeltaTime;
+        drive.targetVelocity = jointVelocity * Mathf.Rad2Deg;
         wheel.xDrive = drive;
     }
 
@@ -131,7 +178,7 @@ public class ArticulationFourWheelController : MonoBehaviour
     private void SetWheelPosition(ArticulationBody wheel, float jointAngle)
     {
         ArticulationDrive drive = wheel.xDrive;
-        drive.target = jointAngle;
+        drive.target = jointAngle * Mathf.Rad2Deg;
         wheel.xDrive = drive;
     }
     private void StopWheel(ArticulationBody wheel)
